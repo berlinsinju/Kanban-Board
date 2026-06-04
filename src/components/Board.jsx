eced8df18b74da475c9d7540b473f5894cfbb33c
Berlin Sinju <sinjugodwin@gmail.com>
2026-06-04 15:10:22 +0530

    Updated the button

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -82,7 +82,7 @@ const Board = ({ setSelectedTask, setOpenModal }) => {
                 border-2 border-dashed
                 rounded-2xl cursor-pointer
                 text-gray-500
-                hover:text-gray-700
+                hover:text-black-700
                 hover:border-gray-400
                 transition 
               "
