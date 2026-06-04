56232ef81180cf208c85fa67f242f5072d897cf0
Berlin Sinju <sinjugodwin@gmail.com>
2026-06-04 15:04:30 +0530

    Updated the logo alignment

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,32 @@ function App() {
       <div className="max-w-7xl mx-auto px-6 py-8">
         <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-6 mb-10">
           {" "}
-          <div className="flex items-center gap-3">
-            <img src={logoImg} alt="Logo" className="w-50" />
+          <div className="flex items-center justify-center md:justify-start gap-3">
+            <img src={logoImg} alt="Logo" className="w-[200px]" />
           </div>
-          <button
-            onClick={() => {
-              setSelectedTask(null);
-              setOpenModal(true);
-            }}
-            className="
-    inline-flex items-center gap-2
-    px-5 py-2.5
-    text-sm font-semibold
-    text-white
-    bg-[var(--button-color)]
-    rounded-xl cursor-pointer
-    shadow-lg shadow-blue-900/30
-    hover:bg-[var(--primary-color)]
-    hover:shadow-lg hover:shadow-blue-900/40
-    transition-all duration-200
-    active:scale-95
-  "
-          >
-            Create Task
-          </button>
+          <div className="flex justify-center md:justify-start">
+            <button
+              onClick={() => {
+                setSelectedTask(null);
+                setOpenModal(true);
+              }}
+              className="
+      inline-flex items-center gap-2
+      px-5 py-2.5
+      text-sm font-semibold
+      text-white
+      bg-[var(--button-color)]
+      rounded-xl cursor-pointer
+      shadow-lg shadow-blue-900/30
+      hover:bg-[var(--primary-color)]
+      hover:shadow-lg hover:shadow-blue-900/40
+      transition-all duration-200
+      active:scale-95
+    "
+            >
+              Create Task
+            </button>
+          </div>
         </div>
 
         <Board setSelectedTask={setSelectedTask} setOpenModal={setOpenModal} />
